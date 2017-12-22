dc00a2346801a663e0ddc3b76946205fb9b577fd
Alex Bigelow <alex.r.bigelow@gmail.com>
2017-12-22 09:26:34 -0700

    Structure detection working

---
+++ structureTests.js
@@ -0,0 +1,102 @@
+/* globals d3, Model, JsDiff */
+
+let tests = [
+  {
+    'tag': 'g',
+    'children': [
+      {
+        'tag': 'rect'
+      }, {
+        'tag': 'text',
+        'children': [
+          {
+            'tag': 'tspan'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    'tag': 'g',
+    'children': [
+      {
+        'tag': 'rect'
+      }, {
+        'tag': 'text',
+        'children': [
+          {
+            'tag': 'tspan'
+          }
+        ]
+      },
+      {
+        'tag': 'rect'
+      }
+    ]
+  },
+  {
+    'tag': 'g',
+    'children': [
+      {
+        'tag': 'g',
+        'children': [
+          {
+            'tag': 'g',
+            'children': [
+              {
+                'tag': 'g',
+                'children': [
+                  {
+                    'tag': 'g',
+                    'children': [
+                      {
+                        'tag': 'g',
+                        'children': [
+                          {
+                            'tag': 'rect'
+                          }
+                        ]
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+];
+tests.forEach(test => {
+  let svg = d3.select('body').append('svg');
+  let testModel = new Model(test);
+  testModel.getEnterFunction().apply(svg.node());
+
+  let learnedModel = new Model();
+  learnedModel.inferStructure(svg.selectAll('svg > ' + test.tag).nodes());
+
+  let testStructure = testModel.getReadableStructureString();
+  let learnedStructure = learnedModel.getReadableStructureString();
+
+  if (testStructure === learnedStructure) {
+    console.log('Passed structure test:');
+    console.log(testStructure);
+  } else {
+    console.warn('Failed structure test:');
+    let diff = JsDiff.diffChars(testStructure, learnedStructure);
+    let partStyles = [];
+    console.log(diff.map(part => {
+      if (part.added) {
+        partStyles.push('color:green');
+      } else if (part.removed) {
+        partStyles.push('color:red');
+      } else {
+        partStyles.push('color:grey');
+      }
+      return '%c' + part.value;
+    }).join(''), ...partStyles);
+  }
+
+  svg.remove();
+});
